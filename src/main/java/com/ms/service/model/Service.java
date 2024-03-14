1f632f11163c6623cc7f093754ccbb25af249159
Fanaina Alves <fanaiina.allves@gmail.com>
2024-03-14 15:19:56 -0300

    Create attributes of class Service.

--- src/main/java/com/ms/service/model/Service.java
+++ src/main/java/com/ms/service/model/Service.java
@@ -1,4 +1,28 @@
 package com.ms.service.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "services")
 public class Service {
+
+    @Id
+    private String serviceID; // id
+    private String serviceName; // nome
+    private String serviceEmail; // email
+    private String serviceStatus; // status
+    private boolean serviceDescription; // descrição
+    private float servicePrice; // preço
+    private int serviceRuntime; // tempo de execução
+    private int serviceTerm; // prazo
+
 }
